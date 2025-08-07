dfba4edd56091541d8e9f0ad024a3d09a029f6f4
DiegoFAlbarracinR <diego@desarrollo.com>
2025-08-07 16:04:16 -0500

    Ejemplo de insercion por lote con foreach

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,21 +1,33 @@
 import { Injectable } from '@nestjs/common';
 import axios, { AxiosInstance } from 'axios';
 import { PokeResponse } from './interfaces/poke-response.interface';
+import { InjectModel } from '@nestjs/mongoose';
+import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
+import { Model } from 'mongoose';
 
 @Injectable()
 export class SeedService {
 
   private readonly axios: AxiosInstance = axios;
-  
-  async executeSeed() {
-    const { data } = await this.axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=600');
 
-    data.results.forEach( ({name, url}) => {
+  constructor(
+    //Para llamar la BD
+    @InjectModel( Pokemon.name )//Se le da el nombre al modelo
+    private readonly pokemonModel: Model<Pokemon>,//se define la Dependecia en este caso el Model que es de moogose con la estructura de nuestro entitiy Pokemon
+  ) {}
+
+  async executeSeed() {
+    const { data } = await this.axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=10');
+
+    data.results.forEach( async({name, url}) => {
       const segments = url.split('/');
       const no: number = +segments[segments.length - 2];
+
+      const pokemon = await this.pokemonModel.create( {name, no} );
+
     } );
 
-    return data.results;
+    return 'Seed Executed';
   }
 
 }
